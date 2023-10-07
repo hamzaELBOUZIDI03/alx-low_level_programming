f56c19b208a393492ccb37b442a0c3ed81bcd17d
ELBOUZIDI <hamzaelbouzidi08@gmail.com>
2023-10-07 05:46:21 -0700

    update 0 P or N file

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* This program generates a random number and checks if it's positive, negative, or zero. */
+/*
+ * main - Determins if a number  is positive or negative or zero
+ *
+ * Return: Always 0 (Success)
+*/
 int main(void)
 {
 int n;
