8a22ce7ba4bfa6d59cf6d321fb1d319e9daf964d
ELBOUZIDI <hamzaelbouzidi08@gmail.com>
2023-10-07 14:13:38 -0700

    push file

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Seucess)
+ *
+*/
+int main(void)
+{
+char aph[26] = "abcdfghijklmnoprstuvwxyz";
+int i;
+for (i = 0; i < 26; i++)
+{
+putchar(aph[i]);
+}
+putchar('\n');
+return (0);
+}
+
