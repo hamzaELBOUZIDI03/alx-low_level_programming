c85e81d55ea9fc146cce3555b9743f9480ca1866
ELBOUZIDI <hamzaelbouzidi08@gmail.com>
2023-10-07 14:26:29 -0700

    pudh file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Seucess)
+ *
+*/
+int main(void)
+{
+int numbers;
+int i;
+for (i = 0; i < numbers; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
+
