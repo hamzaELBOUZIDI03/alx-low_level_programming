cef93f26bceab4406eae23d68973c36e1911eef4
ELBOUZIDI <hamzaelbouzidi08@gmail.com>
2023-10-11 14:07:47 -0700

    push files

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,20 +1,16 @@
 #include "main.h"
-
-
-	/**
-	 * print_alphabet - print all alphabet in lowercase
-	 */
-
-
-	void print_alphabet(void)
-	{
-		char letter;
-
-
-		for (letter = 'a'; letter <= 'z'; letter++)
-			_putchar(letter);
-
-
-		_putchar('\n');
-	}
+/**
+ ** main - check the code
+ *
+ * Return: Always 0.
+*/
+void print_alphabet(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+_putchar(letter);
+}
+_putchar('\n');
+}
 
